294248b0fc89ecb5f00b70601a3ff7fc3e07b18c
Aleksander Afanasyev <afonia.marcii@gmail.com>
2024-12-11 13:45:20 +0100

    feat: implement note creation handler and HTTP server setup

---
+++ handlers/notes.go
@@ -0,0 +1,11 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+)
+
+
+func CreateNote(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Create Note 1")
+}
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+  "notes-api/handlers"
+)
 
 func main() {
-    fmt.Println("Hello, Go!")
+	http.HandleFunc("/notes", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost { // Check for POST method
+			handlers.CreateNote(w, r)
+		} else {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
+
+	log.Println("Server started on: http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("Failed to start server: %v", err)
+	}
 }
